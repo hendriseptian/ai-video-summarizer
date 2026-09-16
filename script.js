6060c319ac947c854f6ccf7e311b072361716ede
hendri <hendriseptian25@gmail.com>
2026-09-16 12:25:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3081,7 +3081,7 @@ function exportPDF() {
         );
 
         pdf.text(
-            "Made with ❤️ for Tyas Oktaviana",
+            "AI Video Summarizer • Made with ❤️ for Tyas Oktaviana",
             105,
             285,
             {
